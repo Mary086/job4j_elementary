39019724364b3f36670a36a5386df91f611a051a
mariazaharutina <mashanya96@mail.ru>
2023-09-05 21:59:36 +0400

    9. Площадь треугольника. [#175474]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        idea += " But I am a newbie.";
+        System.out.println(idea);
+        int year = 2023;
+        String rsl = idea + year;
+        System.out.println(rsl);
+    }
+
+}
