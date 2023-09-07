6cb725af0ab706a7c049beeedc5b8e1860c27f38
mariazaharutina <mashanya96@mail.ru>
2023-09-07 19:44:50 +0400

    4.1. Разбор первой программы. [#159033 #441817]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,9 +1,7 @@
 package ru.job4j;
 public class StudentInfo {
     public static void main(String[] args) {
-        {
             System.out.println("Mary Zakharutina");
             System.out.println("07.10.1996");
-        }
     }
 }
