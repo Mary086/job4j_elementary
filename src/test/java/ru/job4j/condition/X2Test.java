9b13018342c44c7183afa0b6f36f35b13fd42a26
mariazaharutina <mashanya96@mail.ru>
2023-09-29 09:46:34 +0400

    1.2. Входные данные в тесте [#297750 #448437]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -1,6 +1,8 @@
 package ru.job4j.condition;
+
 import static org.assertj.core.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+
 public class X2Test {
     @Test
     void whenA10B0C0X2Then40() {
