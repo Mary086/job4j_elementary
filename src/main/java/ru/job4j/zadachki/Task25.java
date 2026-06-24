0f0df75f26071213a294999a0c79e016aef5701a
maria <mashanya96@mail.ru>
2026-06-24 14:40:21 +0400

    Отработка задания

---
+++ src/main/java/ru/job4j/zadachki/Task25.java
@@ -0,0 +1,13 @@
+package ru.job4j.zadachki;
+
+public class Task25 {
+    public static void isPositive(int number) {
+        if (number > 0) {
+            System.out.println("Да");
+        } else {
+            System.out.println("Нет");
+        }
+    }
+}
+
+
